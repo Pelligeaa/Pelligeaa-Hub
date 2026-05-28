d751dc598077209805e3776f823e5d27b8d8cc20
Pelligeaa <yw42sdrr8t@privaterelay.appleid.com>
2026-05-28 14:33:46 -0400

    Update portfolio.py

--- Pelligeaa-Research/utils/portfolio.py
+++ Pelligeaa-Research/utils/portfolio.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+
+
+def load_portfolio(uploaded_file):
+    if uploaded_file.name.endswith(".csv"):
+        return pd.read_csv(uploaded_file)
+
+    return pd.read_excel(uploaded_file)
