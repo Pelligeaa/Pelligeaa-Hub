34d49329e5c1a8691e9eb6e65b12e006637d0769
Pelligeaa <yw42sdrr8t@privaterelay.appleid.com>
2026-05-28 14:34:25 -0400

    Update config_manager.py

--- Pelligeaa-Research/utils/config_manager.py
+++ Pelligeaa-Research/utils/config_manager.py
@@ -1,1 +1,29 @@
+import json
+import os
 
+CONFIG_FILE = "config/saved_configs.json"
+
+
+
+def save_config(name, config):
+    os.makedirs("config", exist_ok=True)
+
+    configs = {}
+
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "r") as f:
+            configs = json.load(f)
+
+    configs[name] = config
+
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(configs, f, indent=4)
+
+
+
+def load_configs():
+    if not os.path.exists(CONFIG_FILE):
+        return {}
+
+    with open(CONFIG_FILE, "r") as f:
+        return json.load(f)
